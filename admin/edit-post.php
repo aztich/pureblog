6d00d3bf8715e7528a8733ec87bc17836def51b7
Kev Quirk <kev@quirk.cc>
2026-04-02 08:40:06 +0100

    Bug fix in tag-idex cache

--- admin/edit-post.php
+++ admin/edit-post.php
@@ -369,7 +369,7 @@ require __DIR__ . '/../includes/admin-head.php';
         let allTags = [];
         let activeIndex = -1;
 
-        fetch(window.PureblogEditorConfig.basePath + '/content/tag-index.json')
+        fetch(window.PureblogEditorConfig.basePath + '/content/tag-index.json?v=<?= e(PUREBLOG_VERSION) ?>')
             .then(r => r.ok ? r.json() : null)
             .then(data => { if (data) allTags = Object.values(data).map(v => v.name); })
             .catch(() => {});
